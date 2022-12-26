03483e05cbfb105634b939b1d32bd7ded8a8307b
Pugazharasan <32019083+PugazharasanC@users.noreply.github.com>
2022-12-26 11:57:41 +0530

    Session Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.use(express.json())
+const port = process.env.PORT || 8080;
+app.listen(port, function(){
+    console.log(`app is listening port ${port}`);
+})
+
+app.get("/:name",(req,res)=>{
+    const {name} = req.params;
+    res.status(200).send(`name: ${name}`)
+})
+app.get("/",(req,res)=>{
+    res.send(`App is working fine`)
+})
